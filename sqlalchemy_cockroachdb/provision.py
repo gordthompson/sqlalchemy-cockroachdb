228cdcdc936f9235f3eb69a6046d8feb83e00bd8
Gord Thompson <gord@gordthompson.com>
2024-12-05 11:03:03 -0700

    Update provision.py

--- sqlalchemy_cockroachdb/provision.py
+++ sqlalchemy_cockroachdb/provision.py
@@ -1,4 +1,14 @@
+from sqlalchemy import event
+from sqlalchemy.testing.provision import post_configure_engine
 from sqlalchemy.testing.provision import temp_table_keyword_args
+
+
+@post_configure_engine.for_db("cockroachdb")
+def _cockroachdb_post_configure_engine(url, engine, follower_ident):
+    @event.listens_for(engine, "checkout")
+    def checkout(dbapi_con, con_record, con_proxy):
+        cursor = dbapi_con.cursor()
+        cursor.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE")
 
 
 @temp_table_keyword_args.for_db("cockroachdb")
