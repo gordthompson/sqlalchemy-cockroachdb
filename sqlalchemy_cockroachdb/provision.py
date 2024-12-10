5004128e6f792f56c5d1c139749475538a5158ae
Gord Thompson <gord@gordthompson.com>
2024-12-10 09:21:52 -0700

    Update README.asyncpg.md

--- sqlalchemy_cockroachdb/provision.py
+++ sqlalchemy_cockroachdb/provision.py
@@ -6,7 +6,8 @@ from sqlalchemy.testing.provision import update_db_opts
 def _cockroachdb_temp_table_keyword_args(cfg, eng):
     return {"prefixes": ["TEMPORARY"]}
 
+
 @update_db_opts.for_db("cockroachdb")
 def _update_db_opts(db_url, db_opts, options):
     """Set database options (db_opts) for a test database that we created."""
-    db_opts['isolation_level'] = 'SERIALIZABLE'
+    db_opts["isolation_level"] = "SERIALIZABLE"
