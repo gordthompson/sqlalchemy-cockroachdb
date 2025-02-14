9f053d08cc4bb230837ebed5a874657dda7872f6
Gord Thompson <gord@gordthompson.com>
2025-02-14 13:29:59 -0700

    Revert "Disable autocommit_before_ddl for tests"
    This reverts commit 42fb9bbc398fa06a05e3580701ad21dc83991b32.

--- sqlalchemy_cockroachdb/provision.py
+++ sqlalchemy_cockroachdb/provision.py
@@ -1,15 +1,6 @@
-from sqlalchemy import event
-from sqlalchemy.testing.provision import temp_table_keyword_args, post_configure_engine
+from sqlalchemy.testing.provision import temp_table_keyword_args
 
 
 @temp_table_keyword_args.for_db("cockroachdb")
 def _cockroachdb_temp_table_keyword_args(cfg, eng):
     return {"prefixes": ["TEMPORARY"]}
-
-
-@post_configure_engine.for_db("cockroachdb")
-def _cockroachdb_post_configure_engine(url, engine, follower_ident):
-    @event.listens_for(engine, "checkout")
-    def checkout(dbapi_con, con_record, con_proxy):
-        cursor = dbapi_con.cursor()
-        cursor.execute("SET autocommit_before_ddl = off")
